2cbbdf4db9ce3387003c3cae645cd50ea0129f44
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-03 05:34:30 -0300

    components: styled components

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 20px;
+  color: ${(props) => (props.error ? 'red' : '#7159c1')};
+  font-family: Arial, Helvetica, sans-serif;
+
+  small {
+    color: #ee9b43;
+    font-size: 14px;
+    margin-left: 2px;
+  }
+`;
