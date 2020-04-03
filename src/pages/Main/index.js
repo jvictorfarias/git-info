70f9cf2b9b82aa65412b52bff9edaebdea98d533
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-03 07:13:19 -0300

    components: states and api connection

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-export default function Main() {
-  return (
-    <Title error={false}>
-      Main
-      <small>Outra main</small>
-    </Title>
-  );
+import { Container, Form, SubmitButton } from './styles';
+
+import api from '../../services/api';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            <FaPlus color="#fff" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,13 +1,56 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 20px;
-  color: ${(props) => (props.error ? 'red' : '#7159c1')};
-  font-family: Arial, Helvetica, sans-serif;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #f8f8f2;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
 
-  small {
-    color: #ee9b43;
-    font-size: 14px;
-    margin-left: 2px;
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #44475a;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+    color: #44475a;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
+  background: #50fa7b;
+  border: 0;
+  margin-left: 10px;
+  border-radius: 4px;
+  padding: 0 15px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
   }
 `;
